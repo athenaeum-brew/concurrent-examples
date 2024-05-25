32f3d96483e4d9677ec2c1a7f3803323185c912f
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2024-05-25 23:33:13 +0200

    VirtualThreads

---
+++ Example60VirtualThreads.java
@@ -0,0 +1,33 @@
+/*
+   javac --enable-preview --release 22 Example60VirtualThreads.java
+   java --enable-preview Example60VirtualThreads
+ */
+
+public class Example60VirtualThreads {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Create and start a virtual thread
+        Thread vThread = Thread.ofVirtual().start(() -> {
+            System.out.println("Hello from virtual thread!");
+        });
+
+        // Wait for the virtual thread to finish
+        vThread.join();
+
+        // Create and start multiple virtual threads
+        int numThreads = 100;
+        Thread[] threads = new Thread[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = Thread.ofVirtual().start(() -> {
+                System.out.println("Thread " + Thread.currentThread().getId() + " is running.");
+            });
+        }
+
+        // Wait for all virtual threads to finish
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("All virtual threads have finished.");
+    }
+}
